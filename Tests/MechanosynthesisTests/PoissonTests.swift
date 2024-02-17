5a4152eca42a62e1a0223594616cec5dfdf578af
Philip Turner <philipturner.AR@gmail.com>
2024-02-16 23:08:35 -0500

    Potential method for computing atomic forces

--- Tests/MechanosynthesisTests/PoissonTests.swift
+++ Tests/MechanosynthesisTests/PoissonTests.swift
@@ -5,3 +5,8 @@ import Numerics
 // Prove that a multigrid solver with rough mipmap sampling works. This is
 // different from the Real-Space DFT paper, which used a complicated
 // interpolation scheme.
+//
+// The combined electron and ion Hartree potential can be used to compute atomic
+// forces. Interpret the potential as a voltage, and the derivative of voltage
+// is electric field. The electric field exerts a force on the nucleus. The
+// cost is O(1) * O(number of nuclei).
