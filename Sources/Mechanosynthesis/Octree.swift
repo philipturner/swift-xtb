5671962b1013f55ae9cd5b6001c3bef069ef2a3d
Philip Turner <philipturner.AR@gmail.com>
2024-02-18 21:05:57 -0500

    Function for adding child nodes to the octree

--- Sources/Mechanosynthesis/Octree.swift
+++ Sources/Mechanosynthesis/Octree.swift
@@ -9,21 +9,13 @@
 // architectures (CPU and GPU).
 //
 // Stores a set of fragments. Each fragment internally stores a 2x2x2 group
-// of sub-fragments contiguously. It can compute the first derivative at the
+// of sub-fragments contiguously. One can compute the first erivative at the
 // center of the cell for XC functionals.
-
-struct OctreeDescriptor {
-  // The radius of the highest octree level, in powers of 2. All octrees are
-  // centered at the origin.
-  var highestLevelSize: Int
-}
 
 struct Octree {
   // Stores the child cells' data. Even if there aren't child nodes, there are
   // always child grid cells.
   var cellValues: [SIMD8<Float>] = []
-  
-  var highestLevelSize: Int
   
   // If this element terminates the current 2x2x2 cell, the next element
   // is 'nil'. Otherwise, it points to the location right after the children
@@ -33,17 +25,96 @@ struct Octree {
   // of children must be either 0 or 8.
   var linkedList: [(nextElement: UInt32?, childCount: UInt8)] = []
   
-  init(descriptor: OctreeDescriptor) {
+  init() {
     cellValues = [SIMD8.zero]
-    highestLevelSize = descriptor.highestLevelSize
     linkedList = [(nextElement: nil, childCount: 0)]
   }
   
   // Efficient function to add children to several nodes at once.
   mutating func appendChildren(to nodes: [UInt32]) {
-    // Assert that each specified node exists, and its child count is zero.
+    var insertionMarks = [Bool](repeating: false, count: linkedList.count)
     
-    // Initialize the child cells' data to SIMD8<Float>.zero.
+    // Assert that each specified node exists, and its child count is zero.
+    for nodeID in nodes {
+      guard nodeID < linkedList.count else {
+        fatalError("Element does not exist.")
+      }
+      let element = linkedList[Int(nodeID)]
+      guard element.childCount == 0 else {
+        // Converse: Removed children from vacant cell.
+        fatalError("Inserted children into occupied cell.")
+      }
+      insertionMarks[Int(nodeID)] = true
+    }
+    
+    // Create a list of the previous elements that reference the current one.
+    var pointingNodeMap = [UInt32?](repeating: nil, count: linkedList.count)
+    for currentNodeID in linkedList.indices {
+      let element = linkedList[currentNodeID]
+      if let nextElement = element.nextElement {
+        guard pointingNodeMap[Int(nextElement)] == nil else {
+          fatalError("This should never happen.")
+        }
+        pointingNodeMap[Int(nextElement)] = UInt32(currentNodeID)
+      }
+    }
+    
+    // Modify the existing linked list so 'nextElement' already points to
+    // where it should in the new list.
+    var nextElementOffset: Int = .zero
+    for currentNodeID in linkedList.indices {
+      let pointingNodeID = pointingNodeMap[currentNodeID]
+      if let pointingNodeID {
+        let pointingElement = linkedList[Int(pointingNodeID)]
+        guard let nextElement = pointingElement.nextElement else {
+          fatalError("This should never happen.")
+        }
+        
+        let newValue = nextElement + UInt32(nextElementOffset)
+        linkedList[Int(pointingNodeID)].nextElement = newValue
+      }
+      
+      let mark = insertionMarks[currentNodeID]
+      if mark == true {
+        nextElementOffset += 8
+      }
+    }
+    
+    // Create a new array by scanning the current one serially.
+    var newLinkedList: [(nextElement: UInt32?, childCount: UInt8)] = []
+    var newCellValues: [SIMD8<Float>] = []
+    for currentNodeID in linkedList.indices {
+      var element = linkedList[currentNodeID]
+      let mark = insertionMarks[currentNodeID]
+      if mark == true {
+        element.childCount = 8
+      }
+      newLinkedList.append(element)
+      if mark == true {
+        for childID in 0..<8 {
+          var nextElement: Optional = UInt32(newLinkedList.count)
+          if childID == 7 {
+            nextElement = nil
+          }
+          newLinkedList.append((nextElement: nextElement, childCount: 0))
+        }
+      }
+      
+      // Initialize the child cells' data to SIMD8<Float>.zero. Do not modify
+      // the data currently belonging to the expanded cell.
+      let value = cellValues[Int(currentNodeID)]
+      newCellValues.append(value)
+      if mark == true {
+        for _ in 0..<8 {
+          let value: SIMD8<Float> = .zero
+          newCellValues.append(value)
+        }
+      }
+    }
+    
+    // Replace the current arrays with the expanded ones.
+    cellValues = newCellValues
+    linkedList = newLinkedList
   }
   
   // Efficient function to remove children from several nodes at once.
--- Sources/Mechanosynthesis/WaveFunction.swift
+++ Sources/Mechanosynthesis/WaveFunction.swift
@@ -9,6 +9,8 @@ struct WaveFunctionDescriptor {
   // The functional form of the initial guess.
   var atomicOrbital: AtomicOrbital?
   
+  
+  
   // The nuclear position for the initial guess.
   var position: SIMD3<Float>?
   
@@ -20,8 +22,13 @@ struct WaveFunctionDescriptor {
 }
 
 struct WaveFunction {
+  // The radius of the highest octree level, in powers of 2. The octree is
+  // centered at the origin.
+  var highestLevelSize: Int
+  
   init(descriptor: WaveFunctionDescriptor) {
     // Generate an octree and the appropriate bounds from the atomic orbital.
-    // The bounds could change throughout the self-consistent cycle.
+    // The bounds may change throughout the self-consistent cycle.
+    fatalError("Not implemented.")
   }
 }
