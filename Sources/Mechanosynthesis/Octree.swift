5ea1d098519a50f9fc3dcfd8fdc6130746ecd4a1
Philip Turner <philipturner.AR@gmail.com>
2024-02-23 08:06:51 -0500

    Finish the new function for resizing the nodes

--- Sources/Mechanosynthesis/Octree.swift
+++ Sources/Mechanosynthesis/Octree.swift
@@ -25,18 +25,19 @@ public struct OctreeNode {
     centerAndSpacing.w
   }
   
-  // The number of additional elements between this node and its neighbors.
-  public var childrenBefore: UInt32
-  public var childrenAfter: UInt32
+  // The array index of the parent. If there is none, this is 'UInt32.max'.
+  public var parentIndex: UInt32
   
-  // The index in the parent node. Consecutive nodes in memory may not have
-  // contiguous parent indices, because some nodes aren't expanded. Use this to
-  // compute the parent's center, then find the first node with that center.
-  public var indexInParent: UInt8
+  // The array index where the child nodes start. If there are none, this is
+  // 'UInt32.max'.
+  public var branchesIndex: UInt32
   
-  // If any children have grand-children, they are marked in this mask. The
-  // next array elements will be the child nodes in compacted order. If the
-  // child is a leaf node, it isn't marked.
+  // The mask index within the parent node.
+  public var parentMaskIndex: UInt8
+  
+  // If any children have grand-children, they are marked in this mask. Such
+  // children are stored in compacted order. If the child is a leaf node, it
+  // isn't marked.
   public var branchesMask: UInt8
 }
 
@@ -54,29 +55,29 @@ public struct Octree {
     let size = Float(sign: .plus, exponent: sizeExponent, significand: 1)
     let node = OctreeNode(
       centerAndSpacing: SIMD4(origin, size),
-      childrenBefore: 0,
-      childrenAfter: 0,
-      indexInParent: 0,
+      parentIndex: UInt32.max,
+      branchesIndex: UInt32.max,
+      parentMaskIndex: 0,
       branchesMask: 0b0000_0000)
     nodes = [node]
   }
   
   // Efficient function to expand/contract several nodes at once.
-  // - expand: The nodes that should acquire children. Each array element is
-  //           the parent address and the mask of children that should be
-  //           expanded.
-  // - contract: The parents who should no longer have children. The children
-  //             cannot have grandchildren.
+  // - expanded: The nodes that should acquire children. Each array element is
+  //             the parent address and the mask of children that should be
+  //             expanded.
+  // - contracted: The parents who should no longer have children. The children
+  //               cannot have grandchildren.
   // - returns: The old nodes' positions in the new list. If no such position
   //            exists, the array element is `UInt32.max`.
   mutating func resizeNodes(
-    expand: [(UInt32, UInt8)], contract: [UInt32]
+    expanded: [(UInt32, UInt8)], contracted: [UInt32]
   ) -> [UInt32] {
     var insertionMarks = [UInt8](repeating: 0b0000_0000, count: nodes.count)
     var removalMarks = [Bool](repeating: false, count: nodes.count)
     
     // Assert that each specified node exists, and its child count is zero.
-    for (nodeID, expansionMask) in expand {
+    for (nodeID, expansionMask) in expanded {
       guard nodes.indices.contains(Int(nodeID)) else {
         fatalError("Parent does not exist.")
       }
@@ -89,7 +90,7 @@ public struct Octree {
     
     // Assert that each specified node exists, and its children don't have
     // grandchildren.
-    for nodeID in contract {
+    for nodeID in contracted {
       guard nodes.indices.contains(Int(nodeID)) else {
         fatalError("Parent does not exist.")
       }
@@ -103,89 +104,111 @@ public struct Octree {
       removalMarks[Int(nodeID)] = true
     }
     
-    // Modify the existing linked list so 'nextElement' already points to
-    // where it should in the new list.
-    //
-    // TODO: Something similar, but tracking the number of elements expanded or
-    // contracted between a specific node pair. This could get very complicated
-    // if we want it to be an in-place operation.
-    //
-    // Alternatively, try updating from left to right. Store a stack of the last
-    // time a particular level of the hierarchy was hit. That instantly
-    // results in a O(n) childrenBefore computation. Repeat a similar process
-    // for the other traversal direction for childrenAfter.
-    // - WARNING: Need to erase certain nodes of the stack once they go out of
-    //            context (belong to the wrong parent). I think this happens
-    //            automatically when popping the stack to reach a higher level.
-    var nextElementOffset: Int = .zero
-    for currentNodeID in linkedList.indices {
-      let pointingNodeID = pointingNodeMap[currentNodeID]
-      if let pointingNodeID {
-        let pointingElement = linkedList[Int(pointingNodeID)]
-        guard let nextElement = pointingElement.nextElement else {
-          fatalError("This should never happen.")
-        }
-        
-        let newValue = nextElement + UInt32(nextElementOffset)
-        linkedList[Int(pointingNodeID)].nextElement = newValue
+    // Create an array of arrays for the hierarchy levels.
+    var levels: [[OctreeNode]] = []
+    var mappedIndices: [[UInt32]] = []
+    levels.append([])
+    mappedIndices.append([])
+    
+    func traversalFirstPass(nodeID: Int, levelID: Int) {
+      var node = nodes[nodeID]
+      if removalMarks[nodeID] {
+        fatalError("Traversed to a node that should be removed.")
+      }
+      if levelID != 0 {
+        node.parentIndex = UInt32(levels[levelID - 1].count)
+      } else {
+        node.parentIndex = UInt32.max
+      }
+      if levels.count <= levelID + 1 {
+        levels.append([])
+        mappedIndices.append([])
       }
       
-      if insertionMarks[currentNodeID] {
-        nextElementOffset += 8
-      }
-      if removalMarks[currentNodeID] {
-        nextElementOffset -= 1
-      }
-    }
-    
-    // Create a new array by scanning the current one serially.
-    var mappedPositions = [UInt32](repeating: .max, count: linkedList.count)
-    var newLinkedList: [(nextElement: UInt32?, childCount: UInt8)] = []
-    var newMetadata: [SIMD4<Float>] = []
-    for currentNodeID in linkedList.indices {
-      var element = linkedList[currentNodeID]
-      if insertionMarks[currentNodeID] {
-        element.childCount = 8
-      }
-      if removalMarks[currentNodeID] {
-        continue
-      }
-      if !insertionMarks[currentNodeID] {
-        mappedPositions[currentNodeID] = UInt32(newLinkedList.count)
-      }
-      newLinkedList.append(element)
-      if insertionMarks[currentNodeID] {
-        for childID in 0..<8 {
-          var nextElement: Optional = UInt32(newLinkedList.count)
-          if childID == 7 {
-            nextElement = nil
+      var previousBranchesIndex = Int(node.branchesIndex)
+      let nextBranchesIndex = levels[levelID + 1].count
+      
+      // Lookup table for child nodes.
+      let x = SIMD8<Float>(0, 1, 0, 1, 0, 1, 0, 1) * 0.5 - 0.25
+      let y = SIMD8<Float>(0, 0, 1, 1, 0, 0, 1, 1) * 0.5 - 0.25
+      let z = SIMD8<Float>(0, 0, 0, 0, 1, 1, 1, 1) * 0.5 - 0.25
+      let selfIndex = levels[levelID].count
+      
+      if node.branchesMask != 0 {
+        let insertionMask = insertionMarks[nodeID]
+        for branchID in 0..<8 {
+          let branchBit = UInt8(1 << branchID)
+          
+          if node.branchesMask & branchBit != 0 {
+            if removalMarks[previousBranchesIndex] {
+              node.branchesMask ^= branchBit
+              mappedIndices[levelID + 1].append(UInt32.max)
+            } else {
+              traversalFirstPass(
+                nodeID: previousBranchesIndex, levelID: levelID + 1)
+            }
+            previousBranchesIndex += 1
+          } else if insertionMask & branchBit != 0 {
+            node.branchesMask |= branchBit
+            
+            // Construct child position using the lookup table.
+            var xyz = SIMD3(x[branchID], y[branchID], z[branchID])
+            xyz = node.center + xyz * node.spacing
+            
+            // Insert a node for the child.
+            let node = OctreeNode(
+              centerAndSpacing: SIMD4(xyz, node.spacing / 2),
+              parentIndex: UInt32(selfIndex),
+              branchesIndex: UInt32.max,
+              parentMaskIndex: UInt8(branchID),
+              branchesMask: 0b0000_0000)
+            levels[levelID + 1].append(node)
           }
-          newLinkedList.append((nextElement: nextElement, childCount: 0))
         }
       }
       
-      let parentMetadata = metadata[Int(currentNodeID)]
-      newMetadata.append(parentMetadata)
-      if insertionMarks[currentNodeID] {
-        var x = SIMD8<Float>(0, 1, 0, 1, 0, 1, 0, 1) * 0.5 - 0.25
-        var y = SIMD8<Float>(0, 0, 1, 1, 0, 0, 1, 1) * 0.5 - 0.25
-        var z = SIMD8<Float>(0, 0, 0, 0, 1, 1, 1, 1) * 0.5 - 0.25
-        x = x * parentMetadata.w + parentMetadata.x
-        y = y * parentMetadata.w + parentMetadata.y
-        z = z * parentMetadata.w + parentMetadata.z
-        
-        for childID in 0..<8 {
-          let parentSpacing = parentMetadata.w
-          let childPosition = SIMD3(x[childID], y[childID], z[childID])
-          let childSpacing = parentSpacing / 2
-          newMetadata.append(SIMD4(childPosition, childSpacing))
-        }
+      if node.branchesMask != 0 {
+        node.branchesIndex = UInt32(nextBranchesIndex)
+      } else {
+        node.branchesIndex = UInt32.max
+      }
+      mappedIndices[levelID].append(UInt32(selfIndex))
+      levels[levelID].append(node)
+    }
+    traversalFirstPass(nodeID: 0, levelID: 0)
+    
+    // Bookkeeping operations on the indices.
+    let mappedIndexCount = mappedIndices.reduce(0) { $0 + $1.count }
+    guard mappedIndexCount == nodes.count else {
+      fatalError("Unexpected behavior when mapping old nodes to new nodes.")
+    }
+    var levelPrefixSums: [UInt32] = []
+    var levelSum: Int = .zero
+    for level in levels {
+      levelPrefixSums.append(UInt32(levelSum))
+      levelSum += level.count
+    }
+    for levelID in levels.indices {
+      let prefix = levelPrefixSums[levelID]
+      for i in mappedIndices[levelID].indices {
+        mappedIndices[levelID][i] += prefix
       }
     }
     
-    // Replace the current arrays with the expanded ones.
-    linkedList = newLinkedList
-    metadata = newMetadata
-    return mappedPositions
+    // Merge the hierarchy levels into a consecutive array.
+    nodes = []
+    for levelID in levels.indices {
+      for var node in levels[levelID] {
+        if levelID > 0 {
+          node.parentIndex += levelPrefixSums[levelID - 1]
+        }
+        if node.branchesMask != 0 {
+          node.branchesIndex += levelPrefixSums[levelID + 1]
+        }
+        nodes.append(node)
+      }
+    }
+    
+    return mappedIndices.flatMap { $0 }
   }
 }
