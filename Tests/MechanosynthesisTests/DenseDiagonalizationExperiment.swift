9ecc572d016056dbfbeb1d74ad43aa51bd38ec07
Philip Turner <philipturner.AR@gmail.com>
2024-03-03 16:28:16 -0500

    Reformulate the orthogonalization into something suitable for parallelization

--- Tests/MechanosynthesisTests/DenseDiagonalizationExperiment.swift
+++ Tests/MechanosynthesisTests/DenseDiagonalizationExperiment.swift
@@ -64,6 +64,108 @@ final class DenseDiagonalizationExperiment: XCTestCase {
     }
   }
   
+  // Panel-factorized version of Gram-Schmidt, whose computation time scales
+  // quadratically with the number of matrix elements (matrix K * panel count).
+  static func panelGramSchmidtOrthonormalize(
+    matrix: inout [Float], n: Int, panelSize: Int
+  ) {
+    func normalize(electronID: Int) {
+      var norm: Float = .zero
+      for cellID in 0..<n {
+        let value = matrix[electronID * n + cellID]
+        norm += value * value
+      }
+      
+      let normalizationFactor = 1 / norm.squareRoot()
+      for cellID in 0..<n {
+        var value = matrix[electronID * n + cellID]
+        value *= normalizationFactor
+        matrix[electronID * n + cellID] = value
+      }
+    }
+    
+    for electronID in 0..<n {
+      // Normalize the vectors before taking dot products.
+      normalize(electronID: electronID)
+    }
+    
+    var dotProductMatrix = [Float](repeating: .zero, count: panelSize * n)
+    var updateMatrix = [Float](repeating: .zero, count: panelSize * n)
+    
+    var panelStart = 0
+    while panelStart < n {
+      let panelEnd = min(panelStart + panelSize, n)
+      
+      for electronID in panelStart..<panelEnd {
+        let intraPanelID = electronID - panelStart
+        for columnID in 0..<n {
+          dotProductMatrix[intraPanelID * n + columnID] = .zero
+          updateMatrix[intraPanelID * n + columnID] = .zero
+        }
+        
+        // Determine the magnitude of components parallel to previous vectors.
+        for neighborID in 0..<panelStart {
+          var dotProduct: Float = .zero
+          for cellID in 0..<n {
+            let value1 = matrix[electronID * n + cellID]
+            let value2 = matrix[neighborID * n + cellID]
+            dotProduct += value1 * value2
+          }
+          dotProductMatrix[intraPanelID * n + neighborID] = dotProduct
+        }
+        
+        // Negate all components parallel to previous vectors.
+        for cellID in 0..<n {
+          var cellUpdate: Float = .zero
+          for neighborID in 0..<panelStart {
+            let dotProduct = dotProductMatrix[intraPanelID * n + neighborID]
+            let value2 = matrix[neighborID * n + cellID]
+            cellUpdate -= dotProduct * value2
+          }
+          updateMatrix[intraPanelID * n + cellID] += cellUpdate
+        }
+      }
+      
+      for electronID in panelStart..<panelEnd {
+        let intraPanelID = electronID - panelStart
+        
+        // Determine the magnitude of components parallel to previous vectors.
+        for neighborID in panelStart..<electronID {
+          var dotProduct: Float = .zero
+          for cellID in 0..<n {
+            let value1 = matrix[electronID * n + cellID]
+            let value2 = matrix[neighborID * n + cellID]
+            dotProduct += value1 * value2
+          }
+          dotProductMatrix[intraPanelID * n + neighborID] = dotProduct
+        }
+        
+        // Negate all components parallel to previous vectors.
+        for cellID in 0..<n {
+          var cellUpdate: Float = .zero
+          for neighborID in panelStart..<electronID {
+            let dotProduct = dotProductMatrix[intraPanelID * n + neighborID]
+            let value2 = matrix[neighborID * n + cellID]
+            cellUpdate -= dotProduct * value2
+          }
+          updateMatrix[intraPanelID * n + cellID] += cellUpdate
+        }
+        
+        // Add the update to the vector.
+        for cellID in 0..<n {
+          var value1 = matrix[electronID * n + cellID]
+          let cellUpdate = updateMatrix[intraPanelID * n + cellID]
+          value1 += cellUpdate
+          matrix[electronID * n + cellID] = value1
+        }
+        
+        // Rescale the remaining components to unit length.
+        normalize(electronID: electronID)
+      }
+      panelStart += panelSize
+    }
+  }
+  
   // MARK: - Tests
   
   func testGramSchmidt() throws {
@@ -105,6 +207,50 @@ final class DenseDiagonalizationExperiment: XCTestCase {
           XCTAssertEqual(value, 0.0, accuracy: 1e-5)
         }
       }
+    }
+  }
+  
+  func testPanelGramSchmidt() throws {
+    // Generate a pre-determined matrix, test that the current version of
+    // Gram-Schmidt normalizes them.
+    var matrix: [Float] = [
+      7, 6, 5, 4, 3, 2, 1,
+      6, 7, 5, 4, 3, 2, 1,
+      2, -1, -1, 1, 2, 6, 8,
+      0.1, 0.2, 0.5, 0.5, 0.1, 0.2, 0.5,
+      -0.1, 0.2, -0.5, 0.5, -0.1, 0.2, -0.5,
+      -1, -2, -3, -5, -7, -9, -10,
+      69, 23, 9, -48, 7, 1, 9,
+    ]
+    guard matrix.count == 49 else {
+      fatalError("Not a 7x7 matrix.")
+    }
+    Self.panelGramSchmidtOrthonormalize(matrix: &matrix, n: 7, panelSize: 3)
+    
+    var overlapMatrix = [Float](repeating: .zero, count: 49)
+    for electronID in 0..<7 {
+      for neighborID in 0..<7 {
+        var overlap: Float = .zero
+        for cellID in 0..<7 {
+          let value1 = matrix[electronID * 7 + cellID]
+          let value2 = matrix[neighborID * 7 + cellID]
+          overlap += value1 * value2
+        }
+        overlapMatrix[electronID * 7 + neighborID] = overlap
+      }
+    }
+    
+    for electronID in 0..<7 {
+      for neighborID in 0..<7 {
+        let value = overlapMatrix[electronID * 7 + neighborID]
+//        if electronID == neighborID {
+//          XCTAssertEqual(value, 1.0, accuracy: 1e-5)
+//        } else {
+//          XCTAssertEqual(value, 0.0, accuracy: 1e-5)
+//        }
+        print(value, terminator: ", ")
+      }
+      print()
     }
   }
 }
