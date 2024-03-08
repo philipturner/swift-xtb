8b4f32f53ed30ab2daa9a0a744f0a4db64c9d09e
Philip Turner <philipturner.AR@gmail.com>
2024-03-07 20:40:19 -0500

    Test some difficult edge cases for dense diagonalization

--- Tests/MechanosynthesisTests/Algorithms/LinearAlgebraTests.swift
+++ Tests/MechanosynthesisTests/Algorithms/LinearAlgebraTests.swift
@@ -91,8 +91,6 @@ final class LinearAlgebraTests: XCTestCase {
   }
   
   // Reduce the matrix to tridiagonal form.
-  // - TODO: Return the Householder transformations for back-transforming the
-  //   eigenvectors.
   static func tridiagonalize(
     matrix originalMatrix: [Float],
     n: Int
@@ -195,86 +193,147 @@ final class LinearAlgebraTests: XCTestCase {
   
   // Use the utility function from LAPACK to diagonalize a tridiagonal matrix.
   func testDivideAndConquer() throws {
-    // TODO: Also test various 7x7 matrices from previous calculations,
-    // especially the ones with degenerate eigenspaces.
-    let n: Int = 4
-    let originalMatrixA: [Float] = [
+    // The example sourced from Wikipedia.
+    testMatrix([
       4, 1, -2, 2,
       1, 2, 0, 1,
       -2, 0, 3, -2,
       2, 1, -2, -1
+    ], n: 4)
+    
+    var eigenvectors: [Float] = [
+      7, 6, 5, 4, 3, 2, 1,
+      6, 7, 5, 4, 3, 2, 1,
+      2, -1, -1, 1, 2, 6, 8,
+      0.1, 0.2, 0.5, 0.5, 0.1, 0.2, 0.5,
+      -0.1, 0.2, -0.5, 0.5, -0.1, 0.2, -0.5,
+      -1, -2, -3, -5, -7, -9, -10,
+      69, 23, 9, -48, 7, 1, 9,
     ]
-    let originalMatrixT = Self.tridiagonalize(matrix: originalMatrixA, n: n)
-    print()
-    print("original T")
-    for rowID in 0..<n {
-      for columnID in 0..<n {
-        let address = rowID * n + columnID
-        let value = originalMatrixT[address]
-        print(value, terminator: ", ")
+    eigenvectors = Self.transpose(matrix: eigenvectors, n: 7)
+    eigenvectors = Self.modifiedGramSchmidt(matrix: eigenvectors, n: 7)
+    
+    // Well-conditioned eigenspectra without degenerate clusters.
+    testEigenvalues([
+      4, 3, 2, 1, 0.1, -1.3, -2.3
+    ])
+    testEigenvalues([
+      4, 3, 2, 1, 0, -1, -2
+    ])
+    
+    // Problematic for the iterative diagonalization experiment.
+    testEigenvalues([
+      4, 3.01, 3.00, 2.99, 0, -2, -2
+    ])
+    testEigenvalues([
+      4, 3.5, 3, 2, 0, -1, -2
+    ])
+    testEigenvalues([
+      2, 2, 0, -2.99, -3.00, -3.01, -4
+    ])
+    testEigenvalues([
+      2, 1, 0, -2.99, -3.00, -3.01, -4
+    ])
+    
+    // Not problematic for the iterative diagonalization experiment.
+    testEigenvalues([
+      4, 3.01, 3.00, 2.99, 0, -1, -2
+    ])
+    testEigenvalues([
+      4, 3.5, 3, 2, 0, -2, -2
+    ])
+    testEigenvalues([
+      2, 2, 0, -2, -3, -3.5, -4
+    ])
+    
+    func testEigenvalues(_ eigenvalues: [Float]) {
+      var Λ = [Float](repeating: 0, count: 7 * 7)
+      for i in 0..<7 {
+        let address = i * 7 + i
+        let value = eigenvalues[i]
+        Λ[address] = value
       }
-      print()
-    }
-    
-    // Store the tridiagonal matrix in a compact form.
-    var D = [Float](repeating: 0, count: n)
-    var E = [Float](repeating: 0, count: n - 1)
-    for diagonalID in 0..<n {
-      let matrixAddress = diagonalID * n + diagonalID
-      let vectorAddress = diagonalID
-      D[vectorAddress] = originalMatrixT[matrixAddress]
-    }
-    for subDiagonalID in 0..<n - 1 {
-      let rowID = subDiagonalID
-      let columnID = subDiagonalID + 1
-      let matrixAddress = rowID * n + columnID
-      let vectorAddress = rowID
-      E[vectorAddress] = originalMatrixT[matrixAddress]
-    }
-    
-    // Query the workspace size.
-    var JOBZ = CChar(Character("V").asciiValue!)
-    var N = Int32(n)
-    var LDZ = Int32(n)
-    var WORK = [Float](repeating: 0, count: 1)
-    var LWORK = Int32(-1)
-    var IWORK = [Int32](repeating: 0, count: 1)
-    var LIWORK = Int32(-1)
-    var INFO = Int32(0)
-    sstevd_(
-      &JOBZ, &N, nil, nil, nil, &LDZ, &WORK, &LWORK, &IWORK, &LIWORK, &INFO)
-    print("INFO:", INFO)
-    print("LWORK:", WORK[0])
-    print("LIWORK:", IWORK[0])
-    print("D:", D)
-    print("E:", E)
-    
-    // Call into LAPACK.
-    var Z = [Float](repeating: 0, count: n * n)
-    LWORK = Int32(WORK[0])
-    LIWORK = Int32(IWORK[0])
-    WORK = [Float](repeating: 0, count: Int(LWORK))
-    IWORK = [Int32](repeating: 0, count: Int(LIWORK))
-    sstevd_(
-      &JOBZ, &N, &D, &E, &Z, &LDZ, &WORK, &LWORK, &IWORK, &LIWORK, &INFO)
-    print("INFO:", INFO)
-    print("D:", D)
-    print("E:", E)
-    print("Z:", Z)
-    
-    // Check that the eigenvectors produce the eigenvalues.
-    let HΨ = Self.matrixMultiply(
-      matrixA: originalMatrixT, matrixB: Z, transposeB: true, n: n)
-    for electronID in 0..<n {
-      var actualE: Float = .zero
-      for cellID in 0..<n {
-        let address = cellID * n + electronID
-        actualE += HΨ[address] * HΨ[address]
-      }
-      actualE.formSquareRoot()
+      let ΣT = eigenvectors
       
-      let expectedE = D[electronID]
-      XCTAssertEqual(actualE, expectedE.magnitude, accuracy: 1e-5)
+      let ΛΣT = Self.matrixMultiply(
+        matrixA: Λ, transposeA: false,
+        matrixB: ΣT, transposeB: false, n: 7)
+      let A = Self.matrixMultiply(
+        matrixA: ΣT, transposeA: true,
+        matrixB: ΛΣT, transposeB: false, n: 7)
+      let AΣT = Self.matrixMultiply(
+        matrixA: A, transposeA: false,
+        matrixB: ΣT, transposeB: true, n: 7)
+      
+      for electronID in 0..<7 {
+        let expectedEigenvalue = eigenvalues[electronID]
+        var actualEigenvalue: Float = .zero
+        for cellID in 0..<7 {
+          let value = AΣT[cellID * 7 + electronID]
+          actualEigenvalue += value * value
+        }
+        actualEigenvalue.formSquareRoot()
+        XCTAssertLessThan(
+          expectedEigenvalue.magnitude - actualEigenvalue.magnitude, 1e-4)
+      }
+      
+      testMatrix(A, n: 7)
+    }
+    
+    func testMatrix(_ originalMatrixA: [Float], n: Int) {
+      let originalMatrixT = Self.tridiagonalize(matrix: originalMatrixA, n: n)
+      
+      // Store the tridiagonal matrix in a compact form.
+      var D = [Float](repeating: 0, count: n)
+      var E = [Float](repeating: 0, count: n - 1)
+      for diagonalID in 0..<n {
+        let matrixAddress = diagonalID * n + diagonalID
+        let vectorAddress = diagonalID
+        D[vectorAddress] = originalMatrixT[matrixAddress]
+      }
+      for subDiagonalID in 0..<n - 1 {
+        let rowID = subDiagonalID
+        let columnID = subDiagonalID + 1
+        let matrixAddress = rowID * n + columnID
+        let vectorAddress = rowID
+        E[vectorAddress] = originalMatrixT[matrixAddress]
+      }
+      
+      // Query the workspace size.
+      var JOBZ = CChar(Character("V").asciiValue!)
+      var N = Int32(n)
+      var LDZ = Int32(n)
+      var WORK = [Float](repeating: 0, count: 1)
+      var LWORK = Int32(-1)
+      var IWORK = [Int32](repeating: 0, count: 1)
+      var LIWORK = Int32(-1)
+      var INFO = Int32(0)
+      sstevd_(
+        &JOBZ, &N, nil, nil, nil, &LDZ, &WORK, &LWORK, &IWORK, &LIWORK, &INFO)
+      
+      // Call into LAPACK.
+      var Z = [Float](repeating: 0, count: n * n)
+      LWORK = Int32(WORK[0])
+      LIWORK = Int32(IWORK[0])
+      WORK = [Float](repeating: 0, count: Int(LWORK))
+      IWORK = [Int32](repeating: 0, count: Int(LIWORK))
+      sstevd_(
+        &JOBZ, &N, &D, &E, &Z, &LDZ, &WORK, &LWORK, &IWORK, &LIWORK, &INFO)
+      
+      // Check that the eigenvectors produce the eigenvalues.
+      let HΨ = Self.matrixMultiply(
+        matrixA: originalMatrixT, matrixB: Z, transposeB: true, n: n)
+      for electronID in 0..<n {
+        var actualE: Float = .zero
+        for cellID in 0..<n {
+          let address = cellID * n + electronID
+          actualE += HΨ[address] * HΨ[address]
+        }
+        actualE.formSquareRoot()
+        
+        let expectedE = D[electronID]
+        XCTAssertEqual(actualE, expectedE.magnitude, accuracy: 1e-5)
+      }
     }
   }
 }
