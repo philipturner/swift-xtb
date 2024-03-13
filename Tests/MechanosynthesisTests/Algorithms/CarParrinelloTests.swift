86c34130191d9f6ebc374eb017c50cd2ae70b95d
Philip Turner <philipturner.AR@gmail.com>
2024-03-12 20:53:06 -0400

    Use the correct spelling for Car Parrinello

--- Tests/MechanosynthesisTests/Algorithms/CarParrinelloTests.swift
+++ Tests/MechanosynthesisTests/Algorithms/CarParrinelloTests.swift
@@ -5,6 +5,6 @@ import Numerics
 // Objectives:
 // - Execute BOMD on dihydrogen and reproduce the vibrational frequency.
 // - Execute CPMD on dihydrogen and reproduce the vibrational frequency.
-final class CarrParrinelloTests: XCTestCase {
+final class CarParrinelloTests: XCTestCase {
   
 }
