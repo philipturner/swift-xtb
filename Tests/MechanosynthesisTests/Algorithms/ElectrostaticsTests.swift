c86e5e8a8364e5bd5a337a3a0b0f7016d25d7e64
Philip Turner <philipturner.AR@gmail.com>
2024-05-16 10:31:10 -0400

    Inspecting the fragment count

--- Tests/MechanosynthesisTests/Algorithms/ElectrostaticsTests.swift
+++ Tests/MechanosynthesisTests/Algorithms/ElectrostaticsTests.swift
@@ -305,55 +305,105 @@ final class ElectrostaticsTests: XCTestCase {
     // What is the charge enclosed by each hierarchy level?
     func inspect(waveFunction: WaveFunction) {
       let octree = waveFunction.octree
-      
-      // Visualize the fragment count.
-      print("  - total fragments:", terminator: " ")
-      print(waveFunction.fragmentCount, terminator: " ")
-      print("-> \(waveFunction.cellValues.count * 8)")
+      inspectFragmentCount()
+      inspectBoundingBoxes()
       
       // Inspect the bounding boxes.
-      var boundingBoxes: [Float: (SIMD3<Float>, SIMD3<Float>)] = [:]
-      for nodeID in octree.nodes.indices {
-        // Retrieve the node and its boundaries.
-        let node = octree.nodes[nodeID]
-        let boundaryMinimum = node.center - node.spacing / 2
-        let boundaryMaximum = node.center + node.spacing / 2
-        
-        // Retrieve the current bounding box for this level.
-        var boundingBox: (SIMD3<Float>, SIMD3<Float>)
-        if let previousBoundingBox = boundingBoxes[node.spacing] {
-          boundingBox = previousBoundingBox
-        } else {
-          boundingBox = (
-            SIMD3<Float>(repeating: .greatestFiniteMagnitude),
-            SIMD3<Float>(repeating: -.greatestFiniteMagnitude))
+      func inspectBoundingBoxes() {
+        var boundingBoxes: [Float: (SIMD3<Float>, SIMD3<Float>)] = [:]
+        for nodeID in octree.nodes.indices {
+          // Retrieve the node and its boundaries.
+          let node = octree.nodes[nodeID]
+          let boundaryMinimum = node.center - node.spacing / 2
+          let boundaryMaximum = node.center + node.spacing / 2
+          
+          // Retrieve the current bounding box for this level.
+          var boundingBox: (SIMD3<Float>, SIMD3<Float>)
+          if let previousBoundingBox = boundingBoxes[node.spacing] {
+            boundingBox = previousBoundingBox
+          } else {
+            boundingBox = (
+              SIMD3<Float>(repeating: .greatestFiniteMagnitude),
+              SIMD3<Float>(repeating: -.greatestFiniteMagnitude))
+          }
+          
+          // Update the bounding box.
+          boundingBox.0.replace(
+            with: boundaryMinimum, where: boundaryMinimum .< boundingBox.0)
+          boundingBox.1.replace(
+            with: boundaryMaximum, where: boundaryMaximum .> boundingBox.1)
+          boundingBoxes[node.spacing] = boundingBox
         }
         
-        // Update the bounding box.
-        boundingBox.0.replace(
-          with: boundaryMinimum, where: boundaryMinimum .< boundingBox.0)
-        boundingBox.1.replace(
-          with: boundaryMaximum, where: boundaryMaximum .> boundingBox.1)
-        boundingBoxes[node.spacing] = boundingBox
+        // Visualize each level.
+        for coordinateID in 0..<3 {
+          let coordinateNames: [String] = ["x", "y", "z"]
+          let coordinateName = coordinateNames[coordinateID]
+          let indentation: String = "  -"
+          print(indentation, "\(coordinateName):")
+          
+          for level in boundingBoxes.keys.sorted().reversed() {
+            let indentation: String = "    -"
+            let actualLevel = level / 4
+            print(indentation, terminator: " ")
+            print("levels[\(level) -> \(actualLevel)]:", terminator: " ")
+            
+            let box = boundingBoxes[level]!
+            let minimum = box.0[coordinateID]
+            let maximum = box.1[coordinateID]
+            print(minimum, "<", coordinateName, "<", maximum)
+          }
+        }
       }
       
-      // Visualize each level.
-      for coordinateID in 0..<3 {
-        let coordinateNames: [String] = ["x", "y", "z"]
-        let coordinateName = coordinateNames[coordinateID]
-        let indentation: String = "  -"
-        print(indentation, "\(coordinateName):")
+      // Inspect the fragment count.
+      func inspectFragmentCount() {
+        var fragmentCounts: [Float: Int] = [:]
+        for nodeID in octree.nodes.indices {
+          // Retrieve the node.
+          let node = octree.nodes[nodeID]
+          
+          // Determine the node's fragment count.
+          var nodeFragmentCount: Int = .zero
+          for branchID in 0..<8
+          where node.branchesMask[branchID] == UInt8.max {
+            nodeFragmentCount += 8
+          }
+          
+          // Retrieve the current fragment count for this level.
+          var fragmentCount: Int
+          if let previousFragmentCount = fragmentCounts[node.spacing] {
+            fragmentCount = previousFragmentCount
+          } else {
+            fragmentCount = .zero
+          }
+          
+          // Update the fragment count.
+          fragmentCount += nodeFragmentCount
+          fragmentCounts[node.spacing] = fragmentCount
+        }
         
-        for level in boundingBoxes.keys.sorted().reversed() {
+        // Visualize the total fragment count.
+        do {
+          let expectedCount = waveFunction.fragmentCount
+          let occupiedCount = fragmentCounts.values.reduce(0, +)
+          let allocatedCount = waveFunction.cellValues.count * 8
+          print("  - total fragment count:")
+          print("    - expected:", expectedCount)
+          print("    - occupied:", occupiedCount)
+          print("    - allocated:", allocatedCount)
+        }
+        
+        // Visualize each level.
+        print("  - fragment distribution:")
+        for level in fragmentCounts.keys.sorted().reversed() {
           let indentation: String = "    -"
           let actualLevel = level / 4
           print(indentation, terminator: " ")
           print("levels[\(level) -> \(actualLevel)]:", terminator: " ")
           
-          let box = boundingBoxes[level]!
-          let minimum = box.0[coordinateID]
-          let maximum = box.1[coordinateID]
-          print(minimum, "<", coordinateName, "<", maximum)
+          let fragmentCount = fragmentCounts[level]!
+          print(fragmentCount)
         }
       }
     }
