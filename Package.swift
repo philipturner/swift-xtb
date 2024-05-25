570ca856c11d410ff4d3e082eaf64176e161bb31
Philip Turner <philipturner.AR@gmail.com>
2024-05-25 13:11:44 -0400

    Clean up the package manifest

--- Package.swift
+++ Package.swift
@@ -2,57 +2,25 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import class Foundation.ProcessInfo
-
-var targets: [Target] = []
-
-// MARK: - Apple Platforms
-
-var platforms: [SupportedPlatform] = []
-#if canImport(Darwin)
-platforms.append(.macOS(.v13))
-#endif
-
-var cSettings: [CSetting] = []
-var swiftSettings: [SwiftSetting] = []
-
-// MARK: - Package Dependencies
-
-var packageDependencies: [Package.Dependency] = []
-packageDependencies.append(
-  .package(
-    url: "https://github.com/philipturner/swift-numerics", 
-    branch: "Quaternions"))
-
-var targetDependencies: [Target.Dependency] = []
-targetDependencies.append(
-  .product(
-    name: "Numerics",
-    package: "swift-numerics"))
-
-// MARK: - Package Manifest
-
-targets.append(
-  .target(
-    name: "Mechanosynthesis",
-    dependencies: targetDependencies,
-    cSettings: cSettings,
-    swiftSettings: swiftSettings))
-targets.append(
-  .testTarget(
-    name: "MechanosynthesisTests",
-    dependencies: ["Mechanosynthesis"],
-    cSettings: cSettings,
-    swiftSettings: swiftSettings))
 
 let package = Package(
   name: "Mechanosynthesis",
   products: [
     .library(
       name: "Mechanosynthesis",
-      type: .dynamic,
       targets: ["Mechanosynthesis"]),
   ],
-  dependencies: packageDependencies,
-  targets: targets
+  dependencies: [
+    .package(url: "https://github.com/philipturner/swift-numerics", branch: "Quaternions"),
+  ],
+  targets: [
+    .target(
+      name: "Mechanosynthesis",
+      dependencies: [
+        .product(name: "Numerics", package: "swift-numerics"),
+      ]),
+    .testTarget(
+      name: "MechanosynthesisTests",
+      dependencies: ["Mechanosynthesis"]),
+  ]
 )
