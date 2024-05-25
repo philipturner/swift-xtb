ce3c2e443738b08df8a4dc6f2efa1b3be91b90cc
Philip Turner <philipturner.AR@gmail.com>
2024-05-25 11:42:50 -0400

    Some more refactoring to the code, getting ready to parallelize it

--- Sources/Mechanosynthesis/Meshing/Ansatz.swift
+++ Sources/Mechanosynthesis/Meshing/Ansatz.swift
@@ -6,6 +6,7 @@
 //
 
 import Dispatch
+import QuartzCore
 
 /// A configuration for an initial guess.
 public struct AnsatzDescriptor {
@@ -41,7 +42,9 @@ public struct AnsatzDescriptor {
 /// An initial guess at an atom's electronic structure.
 public struct Ansatz {
   public var spinDownOrbitals: [HydrogenicOrbital] = []
+  
   public var spinNeutralOrbitals: [HydrogenicOrbital] = []
+  
   public var spinUpOrbitals: [HydrogenicOrbital] = []
   
   public init(descriptor: AnsatzDescriptor) {
@@ -51,6 +54,8 @@ public struct Ansatz {
       fatalError("Descriptor was incomplete.")
     }
     
+    let checkpoint0 = CACurrentMediaTime()
+    
     // Check that charge and spin are valid.
     guard let spinPolarization = Int(exactly: descriptor.netSpin * 2) else {
       fatalError("Net spin was not divisible by 1/2.")
@@ -59,13 +64,17 @@ public struct Ansatz {
       fatalError("Net charge was greater than nuclear charge.")
     }
     
+    // Find the number of spin-neutral orbitals.
     let electronCount = Int(atomicNumber) - descriptor.netCharge
     guard electronCount % 2 == spinPolarization.magnitude % 2 else {
       fatalError("Spin multiplicity conflicted with electron count.")
     }
     let polarizedOrbitalCount = Int(spinPolarization.magnitude)
     let neutralOrbitalCount = (electronCount - polarizedOrbitalCount) / 2
+    let spinNeutralOccupations = createShellOccupations(
+      electronCount: neutralOrbitalCount)
     
+    // Find the number of spin-polarized orbitals.
     var electronCountDown: Int
     var electronCountUp: Int
     if spinPolarization > 0 {
@@ -77,14 +86,16 @@ public struct Ansatz {
     }
     let spinDownOccupations = createShellOccupations(
       electronCount: electronCountDown)
-    let spinNeutralOccupations = createShellOccupations(
-      electronCount: neutralOrbitalCount)
     let spinUpOccupations = createShellOccupations(
       electronCount: electronCountUp)
+    
+    // Find the effective charge in each electron shell.
     let shellCharges = createEffectiveCharges(
       Z: atomicNumber,
       spinDownOccupations: spinDownOccupations,
       spinUpOccupations: spinUpOccupations)
+    
+    let checkpoint1 = CACurrentMediaTime()
     
     // This loop would be a great place to parallelize across multiple CPU
     // cores. However, we are not permitted to use multicore CPU or hardware
@@ -93,17 +104,20 @@ public struct Ansatz {
     //
     // Even the smallest examples (625 fragments, Z=1) take at least 300 μs.
     
+    var orbitalDescriptors: [HydrogenicOrbitalDescriptor] = []
+    var orbitalSpins: [Float] = []
+    
     for n in shellCharges.indices {
-      let spinDownOccupation = spinDownOccupations[n]
-      let spinNeutralOccupation = spinNeutralOccupations[n]
-      let spinUpOccupation = spinUpOccupations[n]
-      var remainingElectrons = max(spinDownOccupation, spinUpOccupation)
+      let neutralOccupation = spinNeutralOccupations[n]
+      let downOccupation = spinDownOccupations[n]
+      let upOccupation = spinUpOccupations[n]
       
-      var orbitals: [HydrogenicOrbital] = []
+      var remainingOrbitals = max(downOccupation, upOccupation)
+      var remainingNeutralOrbitals = neutralOccupation
       var l = 0
-      while l < n && remainingElectrons > 0 {
+      while l < n && remainingOrbitals > 0 {
         var m = -l
-        while m <= l && remainingElectrons > 0 {
+        while m <= l && remainingOrbitals > 0 {
           var basisFunctionDesc = BasisFunctionDescriptor()
           basisFunctionDesc.Z = shellCharges[n]
           basisFunctionDesc.n = n
@@ -116,22 +130,60 @@ public struct Ansatz {
           orbitalDesc.fragmentCount = descriptor.fragmentCount
           orbitalDesc.nucleusPosition = position
           orbitalDesc.sizeExponent = sizeExponent
-          let orbital = HydrogenicOrbital(descriptor: orbitalDesc)
-          orbitals.append(orbital)
+          orbitalDescriptors.append(orbitalDesc)
           
-          remainingElectrons -= 1
+          if remainingNeutralOrbitals > 0 {
+            orbitalSpins.append(0)
+          } else if spinPolarization > 0 {
+            orbitalSpins.append(0.5)
+          } else {
+            orbitalSpins.append(-0.5)
+          }
+          
+          remainingOrbitals -= 1
+          remainingNeutralOrbitals -= 1
           m += 1
         }
         l += 1
       }
-      guard remainingElectrons == 0 else {
-        fatalError("Unexpected number of remaining electrons.")
+    }
+    
+    let checkpoint2 = CACurrentMediaTime()
+    
+    let orbitals = orbitalDescriptors.map {
+      HydrogenicOrbital(descriptor: $0)
+    }
+    
+    let checkpoint3 = CACurrentMediaTime()
+    
+    for orbitalID in orbitals.indices {
+      let orbital = orbitals[orbitalID]
+      let spin = orbitalSpins[orbitalID]
+      if spin == 0 {
+        spinNeutralOrbitals.append(orbital)
+      } else if spin == 0.5 {
+        spinUpOrbitals.append(orbital)
+      } else {
+        spinDownOrbitals.append(orbital)
       }
-      spinNeutralOrbitals += Array(orbitals[0..<spinNeutralOccupation])
-      spinDownOrbitals += Array(
-        orbitals[spinNeutralOccupation..<spinDownOccupation])
-      spinUpOrbitals += Array(
-        orbitals[spinNeutralOccupation..<spinUpOccupation])
+    }
+    
+    let checkpoint4 = CACurrentMediaTime()
+    
+    // MARK: - Profiling
+    
+    let latencies: [Double] = [
+      checkpoint1 - checkpoint0,
+      checkpoint2 - checkpoint1,
+      checkpoint3 - checkpoint2,
+      checkpoint4 - checkpoint3,
+    ]
+    
+    print()
+    print("Z:", atomicNumber)
+    for latency in latencies {
+      let latencyMicroseconds = Int(latency * 1e6)
+      print(latencyMicroseconds, "μs")
     }
   }
 }
