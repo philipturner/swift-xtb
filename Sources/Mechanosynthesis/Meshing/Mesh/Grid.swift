ce579de3beadd7866d8b5352707b0cb4beaa92df
Philip Turner <philipturner.AR@gmail.com>
2024-05-26 10:41:21 -0400

    Working on the API for Mesh

--- Sources/Mechanosynthesis/Meshing/Mesh/Grid.swift
+++ Sources/Mechanosynthesis/Meshing/Mesh/Grid.swift
@@ -5,13 +5,42 @@
 //  Created by Philip Turner on 5/26/24.
 //
 
-public struct Grid<Element> {
+public struct GridDescriptor<Element> {
   // The lower corner of the bounding box enclosing the cells.
-  var offset: SIMD3<UInt32>
+  public var offset: SIMD3<UInt32> = .zero
   
   // The size of the bounding box enclosing the cells.
-  var dimensions: SIMD3<UInt32>
+  public var dimensions: SIMD3<UInt32>?
+  
+  // An empty element to fill the grid with.
+  public var emptyElement: Element?
+  
+  public init() {
+    
+  }
+}
+
+public struct Grid<Element> {
+  // The lower corner of the bounding box enclosing the cells.
+  public var offset: SIMD3<UInt32> = .zero
+  
+  // The size of the bounding box enclosing the cells.
+  public var dimensions: SIMD3<UInt32>?
   
   // A uniform grid of elements.
-  var cells: [Element]
+  public var cells: [Element]
+  
+  public init(descriptor: GridDescriptor<Element>) {
+    guard let dimensions = descriptor.dimensions,
+          let emptyElement = descriptor.emptyElement else {
+      fatalError("Descriptor was incomplete.")
+    }
+    guard all(dimensions .>= 0) else {
+      fatalError("Cannot have negative dimensions.")
+    }
+    
+    // Allocate an array for the cells.
+    let cellCount = dimensions[0] * dimensions[1] * dimensions[2]
+    cells = Array(repeating: emptyElement, count: Int(cellCount))
+  }
 }
--- Sources/Mechanosynthesis/Meshing/Mesh/Voxel.swift
+++ Sources/Mechanosynthesis/Meshing/Mesh/Voxel.swift
@@ -5,20 +5,34 @@
 //  Created by Philip Turner on 5/26/24.
 //
 
-// A section of real space larger than 1 cubic Bohr.
-public struct CoarseVoxel {
-  // A multigrid of coarse cells, arranged in Morton order in 2x2x2 chunks.
+public struct CoarseVoxelDescriptor {
+  // Required. A uniform grid of fine voxels.
   //
-  // The levels are ordered from coarsest to finest. Their offsets are relative
-  // to the lower corner of this voxel.
-  var coarseLevels: [Grid<SIMD8<Float>>]
+  // The grid's offset is relative to the lower corner of this voxel. The
+  // bounding box must fall within [0, 2^sizeExponent).
+  public var fineVoxels: Grid<FineVoxel>?
+  
+  // Required. The power-2 size of the voxel, in Bohr.
+  public var sizeExponent: Int?
 }
 
-// A section of real space equal to 1 cubic Bohr.
-public struct FineVoxel {
-  // A multigrid of fine cells, arranged in Morton order in 2x2x2 chunks.
+// A large region of real space.
+public struct CoarseVoxel {
+  // A multigrid of 2x2x2 chunks of cells.
   //
   // The levels are ordered from coarsest to finest. Their offsets are relative
   // to the lower corner of this voxel.
-  var fineLevels: [Grid<SIMD8<Float>>]
+  public var coarseLevels: [Grid<SIMD8<Float>>] = []
+  
+  // The size of the spanned region of real space.
+  public var spacing: Int
+}
+
+// A region of real space spanning 1 cubic Bohr.
+public struct FineVoxel {
+  // A multigrid of 2x2x2 chunks of cells.
+  //
+  // The levels are ordered from coarsest to finest. Their offsets are relative
+  // to the lower corner of this voxel.
+  public var fineLevels: [Grid<SIMD8<Float>>] = []
 }
