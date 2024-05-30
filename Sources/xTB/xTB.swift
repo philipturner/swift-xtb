d74d10d52d3652d85aeada7f84d72688731f1393
Philip Turner <philipturner.AR@gmail.com>
2024-05-29 20:31:26 -0400

    Created design specifications for this Swift module

--- Sources/xTB/xTB.swift
+++ Sources/xTB/xTB.swift
@@ -5,5 +5,21 @@
 //  Created by Philip Turner on 5/29/24.
 //
 
-// Goal: Create an API almost identical to MM4ForceField, just without
-// requiring any parameters.
+// Goal: Create an API almost identical to MM4ForceField, just
+// without requiring an equivalent to 'MM4Parameters'.
+//
+// Design Specifications:
+//
+// It should include a built-in energy minimizer:
+// - likely implemented on the Swift side
+// - copying the FIRE parameters used in the 'xtb' program
+//
+// It should automatically convert between nanomechanical (SI) units and atomic
+// units. The public interface should be in nanomechanical units.
+//
+// 64-bit quantities should be exposed as 32-bit in the public API.
+
+// First step: Dedicated Swift wrapper over the C API, similar to
+// swift-openmm.
+//
+// The precursor will leave 64-bit quantities unchanged.
