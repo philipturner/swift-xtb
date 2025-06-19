77f7167f8274187e9fd45ed2136de5c0eba81092
Philip Turner <philipturner.AR@gmail.com>
2025-06-19 09:38:02 -0400

    Finished the long process of updating Homebrew

--- install-libraries.sh
+++ install-libraries.sh
@@ -1,8 +1,10 @@
 # Step into the build folder, to isolate the immediate effects of file
 # processing operations from the top-level folder.
-if [ ! -d ".build" ]; then
-  mkdir .build
-fi
+mkdir .build
 cd .build
 
-echo "Hello world"
+# Download each binary dependency from the Internet.
+#
+# Installing 'xtb' may take ~30 minutes, if you haven't updated Homebrew
+# dependencies in a long time.
+brew install xtb
