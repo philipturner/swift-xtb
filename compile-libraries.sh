6ddeefb5a8f187b6992a07ba7cb3397f0b936cd3
Philip Turner <philipturner.AR@gmail.com>
2025-06-20 14:55:23 -0400

    Fixed the issues with downloading the binaries

--- compile-libraries.sh
+++ compile-libraries.sh
@@ -8,3 +8,17 @@
 #   - Get the 'xtb' executable running from this mutated install.
 # - Compile all necessary dependencies from source, using v0.3.2 (before the
 #   bug fix I'm seeking).
+mkdir .build
+cd .build
+
+# Links referenced in the Homebrew installer:
+#
+# https://github.com/grimme-lab/homebrew-qc/releases/download/mctc-lib-0.3.2_1/mctc-lib-0.3.2_1.arm64_sequoia.bottle.tar.gz
+# https://github.com/grimme-lab/homebrew-qc/releases/download/multicharge-0.3.0/multicharge-0.3.0.arm64_sequoia.bottle.tar.gz
+# https://github.com/grimme-lab/homebrew-qc/releases/download/dftd4-3.7.0/dftd4-3.7.0.arm64_sequoia.bottle.tar.gz
+# https://github.com/grimme-lab/homebrew-qc/releases/download/xtb-6.7.1/xtb-6.7.1.arm64_sequoia.bottle.tar.gz
+
+curl -OsL "https://github.com/grimme-lab/homebrew-qc/releases/download/mctc-lib-0.3.2_1/mctc-lib-0.3.2_1.arm64_sequoia.bottle.tar.gz"
+curl -OsL "https://github.com/grimme-lab/homebrew-qc/releases/download/xtb-6.7.1/xtb-6.7.1.arm64_sequoia.bottle.tar.gz"
+tar -xzf "mctc-lib-0.3.2_1.arm64_sequoia.bottle.tar.gz"
+tar -xzf "xtb-6.7.1.arm64_sequoia.bottle.tar.gz"
