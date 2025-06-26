f8f603df47ff9ead5549d1d17ec3d240c6746880
Philip Turner <philipturner.AR@gmail.com>
2025-06-26 14:06:07 -0400

    First study of these more detailed statistics

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -44,7 +44,7 @@ let worked = FileManager.default.changeCurrentDirectoryPath(path)
 guard worked else {
   fatalError("Could not redirect gfnff_topo directory.")
 }
-xTB_Environment.verbosity = .minimal
+xTB_Environment.verbosity = .full
 //xTB_Environment.setOutput("/dev/null")
 
 // Select the system.
@@ -85,3 +85,114 @@ for _ in 0..<10 {
 let formattedLatency = String(format: "%.1f", minLatency * 1000)
 print()
 print("minimum latency:", formattedLatency, "ms")
+
+
+
+// Largest system - OpenBLAS
+
+/*
+ SCC (total)                   0 d,  0 h,  0 min,  3.643 sec
+ SCC setup                      ...        0 min,  0.001 sec (  0.032%)
+ Dispersion                     ...        0 min,  0.006 sec (  0.176%)
+ classical contributions        ...        0 min,  0.001 sec (  0.023%)
+ integral evaluation            ...        0 min,  0.025 sec (  0.686%)
+ iterations                     ...        0 min,  3.339 sec ( 91.666%)
+ molecular gradient             ...        0 min,  0.263 sec (  7.219%)
+ printout                       ...        0 min,  0.007 sec (  0.196%)
+
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+         ::                     SUMMARY                     ::
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+         :: total energy            -412.064362227315 Eh    ::
+         :: gradient norm              0.725113756878 Eh/a0 ::
+         :: HOMO-LUMO gap              0.884850477748 eV    ::
+         ::.................................................::
+         :: SCC energy              -417.574064126950 Eh    ::
+         :: -> isotropic ES            0.079789399513 Eh    ::
+         :: -> anisotropic ES          0.087013227315 Eh    ::
+         :: -> anisotropic XC          0.112741599146 Eh    ::
+         :: -> dispersion             -0.739231070380 Eh    ::
+         :: repulsion energy           5.509701899634 Eh    ::
+         :: add. restraining           0.000000000000 Eh    ::
+         :: total charge               0.000000000098 e     ::
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+actual latency: 3647.7 ms
+energy: -1796496.862 zJ
+orbitals: 776 776
+ */
+
+
+
+// Largest System - Accelerate
+
+/*
+ SCC (total)                   0 d,  0 h,  0 min,  1.817 sec
+ SCC setup                      ...        0 min,  0.001 sec (  0.056%)
+ Dispersion                     ...        0 min,  0.007 sec (  0.376%)
+ classical contributions        ...        0 min,  0.001 sec (  0.050%)
+ integral evaluation            ...        0 min,  0.025 sec (  1.399%)
+ iterations                     ...        0 min,  1.517 sec ( 83.510%)
+ molecular gradient             ...        0 min,  0.261 sec ( 14.342%)
+ printout                       ...        0 min,  0.005 sec (  0.264%)
+
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+         ::                     SUMMARY                     ::
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+         :: total energy            -412.064362227315 Eh    ::
+         :: gradient norm              0.725113756878 Eh/a0 ::
+         :: HOMO-LUMO gap              0.884850477748 eV    ::
+         ::.................................................::
+         :: SCC energy              -417.574064126949 Eh    ::
+         :: -> isotropic ES            0.079789399513 Eh    ::
+         :: -> anisotropic ES          0.087013227315 Eh    ::
+         :: -> anisotropic XC          0.112741599146 Eh    ::
+         :: -> dispersion             -0.739231070380 Eh    ::
+         :: repulsion energy           5.509701899634 Eh    ::
+         :: add. restraining           0.000000000000 Eh    ::
+         :: total charge               0.000000000098 e     ::
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+actual latency: 1821.9 ms
+energy: -1796496.862 zJ
+orbitals: 776 776
+ */
+
+
+
+// Largest System - Main Branch
+
+/*
+ SCC (total)                   0 d,  0 h,  0 min,  1.485 sec
+ SCC setup                      ...        0 min,  0.001 sec (  0.064%)
+ Dispersion                     ...        0 min,  0.006 sec (  0.387%)
+ classical contributions        ...        0 min,  0.000 sec (  0.025%)
+ integral evaluation            ...        0 min,  0.023 sec (  1.561%)
+ iterations                     ...        0 min,  1.348 sec ( 90.726%)
+ molecular gradient             ...        0 min,  0.104 sec (  7.033%)
+ printout                       ...        0 min,  0.003 sec (  0.201%)
+
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+         ::                     SUMMARY                     ::
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+         :: total energy            -412.064362216177 Eh    ::
+         :: gradient norm              0.724468099442 Eh/a0 ::
+         :: HOMO-LUMO gap              0.884850544720 eV    ::
+         ::.................................................::
+         :: SCC energy              -417.574064115811 Eh    ::
+         :: -> isotropic ES            0.079789399330 Eh    ::
+         :: -> anisotropic ES          0.087013227134 Eh    ::
+         :: -> anisotropic XC          0.112741599357 Eh    ::
+         :: -> dispersion             -0.739231070457 Eh    ::
+         :: repulsion energy           5.509701899634 Eh    ::
+         :: add. restraining           0.000000000000 Eh    ::
+         :: total charge               0.000000000098 e     ::
+         :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+actual latency: 1490.0 ms
+energy: -1796496.861 zJ
+orbitals: 776 776
+ */
