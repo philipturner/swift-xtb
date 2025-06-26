69b2b109dfd85fb897a0a4e086febf2c5a56f864
Philip Turner <philipturner.AR@gmail.com>
2025-06-26 09:46:41 -0400

    Keeping track of the data somewhere else

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -8,6 +8,49 @@
 import Foundation
 import xTB
 
+
+
+// TODO: Move this data over to Google Sheets. Except for keeping track of the
+// correct energy for GFN2-xTB with 233, which belongs in this script.
+
+
+
+// Energy - GFN-FF
+//
+// System | 6.7.1 OpenBLAS  | 6.7.1 Accelerate | Default OpenMP  | Fixed OMP, 1 thread | Fixed OMP, 8 threads |
+// ------ | --------------- | ---------------- | --------------- | ------------------- | -------------------- |
+// 122    |   -57126.394 zJ |    -57126.394 zJ |   -57126.394 zJ |
+// 222    |  -100762.569 zJ |   -100762.569 zJ |  -100762.569 zJ |
+// 233    |  -208433.751 zJ |   -208433.751 zJ |  -208433.751 zJ |
+
+// Latency - GFN-FF
+//
+// System | 6.7.1 OpenBLAS  | 6.7.1 Accelerate | Default OpenMP  | Fixed OMP, 1 thread | Fixed OMP, 8 threads |
+// ------ | --------------- | ---------------- | --------------- | ------------------- | -------------------- |
+// 122    |          1.6 ms |           1.5 ms |          1.2 ms |
+// 222    |          2.9 ms |           3.0 ms |          2.3 ms |
+// 233    |         10.0 ms |           9.6 ms |          7.1 ms |
+
+
+
+// Energy - GFN2-xTB
+//
+// System | 6.7.1 OpenBLAS  | 6.7.1 Accelerate | Default OpenMP  | Fixed OMP, 1 thread | Fixed OMP, 8 threads |
+// ------ | --------------- | ---------------- | --------------- | ------------------- | -------------------- |
+// 122    |  -452321.592 zJ |   -452321.592 zJ |  -452321.592 zJ |
+// 222    |  -841344.658 zJ |   -841344.658 zJ |  -841344.658 zJ |
+// 233    |         crashes |          crashes |         crashes |
+
+// Latency - GFN2-xTB
+//
+// System | 6.7.1 OpenBLAS  | 6.7.1 Accelerate | Default OpenMP  | Fixed OMP, 1 thread | Fixed OMP, 8 threads |
+// ------ | --------------- | ---------------- | --------------- | ------------------- | -------------------- |
+// 122    |        268.3 ms |          70.5 ms |         61.9 ms |
+// 222    |        870.1 ms |         264.8 ms |        216.4 ms |
+// 233    |         crashes |          crashes |         crashes |
+
+
+
 // TODO: Make a new script, 'run.sh', that queries the core count and sets
 // these environment variables. Then, it calls 'swift run -Xswiftc -Ounchecked'.
 
@@ -20,9 +63,11 @@ if let cString2 {
   print("OMP_NUM_THREADS:", String(cString: cString2))
 }
 
+/*
 // Prepare the environment for maximum performance with xTB.
 setenv("OMP_STACKSIZE", "2G", 1)
 setenv("OMP_NUM_THREADS", "8", 1) // replace '8' with number of P-cores
+ */
 
 // Suppress unwanted output from GFN-FF.
 let url = FileManager.default.temporaryDirectory
@@ -35,7 +80,7 @@ xTB_Environment.verbosity = .muted
 xTB_Environment.setOutput("/dev/null")
 
 // Select the system.
-let system: [SIMD4<Float>] = diamondSystem222
+let system: [SIMD4<Float>] = diamondSystem122
 
 // Create the calculator.
 var calculatorDesc = xTB_CalculatorDescriptor()
@@ -43,24 +88,32 @@ calculatorDesc.atomicNumbers = system.map { UInt8($0.w) }
 calculatorDesc.positions = system.map {
   SIMD3($0.x, $0.y, $0.z)
 }
+calculatorDesc.hamiltonian = .forceField
 let calculator = xTB_Calculator(descriptor: calculatorDesc)
 
 // Run just one loop iteration.
-for _ in 0..<1 {
+var minLatency: Double = 1_000_000
+for _ in 0..<10 {
   calculator.molecule.positions = system.map {
     SIMD3($0.x, $0.y, $0.z)
   }
   
   let checkpoint0 = Date()
-  let energy = calculator.energy * xTB_HartreePerZJ
+  let energy = calculator.energy
   let checkpoint1 = Date()
   let latency = checkpoint1.timeIntervalSince(checkpoint0)
+  minLatency = min(latency, minLatency)
   
   let formattedLatency = String(format: "%.1f", latency * 1000)
   print()
   print("actual latency:", formattedLatency, "ms")
   
-  let formattedEnergy = String(format: "%.7f", energy)
-  print("energy:", formattedEnergy, "Ha")
+  let formattedEnergy = String(format: "%.3f", energy)
+  print("energy:", formattedEnergy, "zJ")
   print("orbitals:", calculator.orbitals.count, calculator.orbitals.eigenvalues.count)
 }
+
+// Summarize the results
+let formattedLatency = String(format: "%.1f", minLatency * 1000)
+print()
+print("minimum latency:", formattedLatency, "ms")
