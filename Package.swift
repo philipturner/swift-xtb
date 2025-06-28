c88f182137d3c96b85432fb469961a98b7f6008b
Philip Turner <philipturner.AR@gmail.com>
2025-06-28 16:58:48 -0400

    Basic changes to the package manifest

--- Package.swift
+++ Package.swift
@@ -1,8 +1,19 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.1
 
 import PackageDescription
+
+var linkerSettings: [LinkerSetting] = []
+
+// Best attempt at scoping this peculiar search path to macOS for now. MM4
+// handles it differently, with an environment variable for the path.
+#if os(macOS)
 import class Foundation.FileManager
+
+linkerSettings += [
+  .unsafeFlags(["-L\(FileManager.default.currentDirectoryPath)"]),
+  .linkedLibrary("xtb_accelerate") // change to 'xtb' before supporting Windows
+]
+#endif
 
 let package = Package(
   name: "swift-xtb",
@@ -11,19 +22,12 @@ let package = Package(
     .target(
       name: "C_xTB",
       dependencies: [],
-      linkerSettings: [
-        .unsafeFlags(["-L\(FileManager.default.currentDirectoryPath)"]),
-        .linkedLibrary("xtb_accelerate")
-      ]),
+      linkerSettings: linkerSettings),
     .target(
       name: "xTB",
-      dependencies: [
-        "C_xTB"
-      ]),
+      dependencies: ["C_xTB"]),
     .executableTarget(
       name: "Workspace",
-      dependencies: [
-        "xTB"
-      ]),
+      dependencies: ["xTB"]),
   ]
 )
