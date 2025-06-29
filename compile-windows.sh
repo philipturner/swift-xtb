b78229138f76d125fb1364e600953ba1d6c4cf49
Philip Turner <philipturner.AR@gmail.com>
2025-06-29 15:31:02 -0400

    Located the binaries

---
+++ compile-windows.sh
@@ -0,0 +1,19 @@
+# Binary dependencies of xtb.exe/libxtb.dll:
+#
+# libgcc_s_seh-1.dll
+# - location: /mingw64/bin
+# libgfortran-5.dll
+# - location: /mingw64/bin
+# KERNEL32.dll
+# - not problematic
+# msvcrt.dll
+# - not problematic
+# libgomp-1.dll
+# - location: /mingw64/bin
+# libopenblas.dll
+# - location: /mingw64/bin
+#
+# Issue with mingw64:
+# - Git Bash terminal: C:\Program Files\Git\mingw64
+# - MSYS2 MINGW64 terminal: C:\msys64\mingw64
+# - VS Code terminal: not even Bash (cannot do 'export' command)
