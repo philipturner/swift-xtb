1a5f87852b1ed4b445875b6f31bceaa890675ade
Philip Turner <philipturner.AR@gmail.com>
2025-06-29 17:45:00 -0400

    Preparing first attempt at a full build

--- compile-windows.sh
+++ compile-windows.sh
@@ -3,6 +3,7 @@
 # For now, invoke the compiler from a regular command line, using the binaries
 # already installed on the PC. After that, we can trace it back to CLI commands
 # that download the GNU/CMake/Ninja dependencies.
+export MINGW64_DIR="/c/msys64/mingw64"
 
 # Enter the build folder.
 mkdir .build
@@ -12,7 +13,17 @@ git clone --single-branch --branch fix-gfnff-output https://github.com/philiptur
 
 # Compile the code from source.
 cd xtb
-# TODO: Invoke CMake
+PATH="$MINGW64_DIR/bin:$PATH" cmake \
+ -B build \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DCMAKE_C_COMPILER="gcc" \
+ -DCMAKE_Fortran_COMPILER="gfortran" \
+ -DWITH_TBLITE=OFF \
+ -DWITH_CPCMX=OFF
+PATH="$MINGW64_DIR/bin:$PATH" ninja \
+  -C build
+PATH="$MINGW64_DIR/bin:$PATH" ninja \
+  -C build test
 
 # Purge any copied binaries.
 cd build
@@ -24,7 +35,6 @@ rm -rf "libquadmath-0.dll"
 rm -rf "libwinpthread-1.dll"
 
 # Copy the binaries into the folder.
-export MINGW64_DIR="/c/msys64/mingw64"
 cp "$MINGW64_DIR/bin/libgcc_s_seh-1.dll" "libgcc_s_seh-1.dll"
 cp "$MINGW64_DIR/bin/libgfortran-5.dll" "libgfortran-5.dll"
 cp "$MINGW64_DIR/bin/libgomp-1.dll" "libgomp-1.dll"
