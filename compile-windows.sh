adba5de286be29ae9752c2d2c2951ee7d5a6222b
Philip Turner <philipturner.AR@gmail.com>
2025-06-29 15:57:19 -0400

    Picking apart the binary dependency chain

--- compile-windows.sh
+++ compile-windows.sh
@@ -1,19 +1,61 @@
-# Binary dependencies of xtb.exe/libxtb.dll:
+# === Binary dependencies of xtb.exe/libxtb.dll ===
 #
 # libgcc_s_seh-1.dll
-# - location: /mingw64/bin
+# - installed at both Git Bash and MSYS2 MINGW64
+# - dependencies:
+#   - KERNEL32.dll
+#   - msvcrt.dll
+#   - libwinpthread-1.dll
 # libgfortran-5.dll
-# - location: /mingw64/bin
+# - installed at MSYS2 MINGW64
+# - dependencies:
+#   - libquadmath-0.dll
+#   - ADVAPI32.dll
+#   - libgcc_s_seh-1.dll
+#   - KERNEL32.dll
+#   - msvcrt.dll
+#   - libwinpthread-1.dll
 # KERNEL32.dll
-# - not problematic
+# - not seen in any folders
 # msvcrt.dll
-# - not problematic
+# - not seen in any folders
 # libgomp-1.dll
-# - location: /mingw64/bin
+# - installed at MSYS2 MINGW64
+# - dependencies:
+#   - TODO
 # libopenblas.dll
-# - location: /mingw64/bin
+# - installed at MSYS2 MINGW64
+# - dependencies:
+#   - TODO
+#
+# === Secondary dependencies ===
+#
+# libquadmath-0.dll
+# - installed at MSYS2 MINGW64
+# - dependencies:
+#   - TODO
+# ADVAPI32.dll
+# - not seen in any folders
+# libwinpthread-1.dll
+# - installed at both Git Bash and MSYS2 MINGW64
+#
+# === Commentary ===
 #
 # Issue with mingw64:
 # - Git Bash terminal: C:\Program Files\Git\mingw64
 # - MSYS2 MINGW64 terminal: C:\msys64\mingw64
 # - VS Code terminal: not even Bash (cannot do 'export' command)
+
+# Enter the build folder
+cd .build
+cd xtb
+cd build
+
+# Copy the binaries into the folder.
+export MINGW64_DIR="/c/msys64/mingw64"
+cp "$MINGW64_DIR/bin/libgcc_s_seh-1.dll" "libgcc_s_seh-1.dll"
+cp "$MINGW64_DIR/bin/libgfortran-5.dll" "libgfortran-5.dll"
+cp "$MINGW64_DIR/bin/libgomp-1.dll" "libgomp-1.dll"
+cp "$MINGW64_DIR/bin/libopenblas.dll" "libopenblas.dll"
+
+./xtb.exe --version
