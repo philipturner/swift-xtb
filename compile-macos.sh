51e3eeea16076ef41ab52e304b262881c3779ba4
Philip Turner <philipturner.AR@gmail.com>
2025-06-29 10:53:31 -0400

    Exploring the impact of compiler flags on performance

--- compile-macos.sh
+++ compile-macos.sh
@@ -3,11 +3,18 @@ cd .build
 rm -rf xtb
 git clone --single-branch --branch fix-gfnff-output https://github.com/philipturner/xtb
 
+# Flags to explore:
+# impact of '-fopenmp'
+# impact of '-O0, -O1, -O2, -O3'
+# impact of 'march/mavx' for certain vector instructions
+# impact of flags to enable loop unrolling/vectorization
+
 cd xtb
 cmake -B build \
  -DCMAKE_BUILD_TYPE=Release \
  -DCMAKE_C_COMPILER="gcc-15" \
  -DCMAKE_Fortran_COMPILER="gfortran-15" \
+ -DCMAKE_Fortran_FLAGS="YEET" \
  -DWITH_TBLITE=OFF \
  -DWITH_CPCMX=OFF \
  -DBLA_VENDOR=Apple
