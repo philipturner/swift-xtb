b18da80cdb10cdc60d6a25b7b9ed6e197c05f729
Philip Turner <philipturner.AR@gmail.com>
2025-06-29 13:15:08 -0400

    Done investigating performance on macOS for now

--- compile-macos.sh
+++ compile-macos.sh
@@ -3,15 +3,9 @@ cd .build
 rm -rf xtb
 git clone --single-branch --branch fix-gfnff-output https://github.com/philipturner/xtb
 
-# Flags to explore:
-# try changing to CMake Debug mode
-# restrict to 1 OpenMP thread to isolate changes to single-threaded performance
-# impact of 'mtune' (https://stackoverflow.com/a/75739441)
-# impact of flags to enable loop unrolling/vectorization
-
 cd xtb
 cmake -B build \
- -DCMAKE_BUILD_TYPE=Debug \
+ -DCMAKE_BUILD_TYPE=Release \
  -DCMAKE_C_COMPILER="gcc-15" \
  -DCMAKE_Fortran_COMPILER="gfortran-15" \
  -DWITH_TBLITE=OFF \
