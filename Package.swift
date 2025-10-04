4c34950313ca87635987f6f3ee9e202e0e5e7f52
Philip Turner <philipturner.AR@gmail.com>
2025-10-04 19:16:38 -0400

    Major cleanups

--- Package.swift
+++ Package.swift
@@ -1,28 +1,14 @@
 // swift-tools-version: 6.1
 
 import PackageDescription
-import class Foundation.ProcessInfo
-
-var linkerSettings: [LinkerSetting] = []
-
-if let path = ProcessInfo.processInfo.environment["XTB_LIBRARY_PATH"] {
-  linkerSettings += [
-    .unsafeFlags(["-L\(path)"]),
-    .linkedLibrary("xtb"),
-  ]
-}
 
 let package = Package(
   name: "swift-xtb",
   dependencies: [],
   targets: [
     .target(
-      name: "C_xTB",
-      dependencies: [],
-      linkerSettings: linkerSettings),
-    .target(
       name: "xTB",
-      dependencies: ["C_xTB"]),
+      dependencies: []),
     .executableTarget(
       name: "Workspace",
       dependencies: ["xTB"]),
